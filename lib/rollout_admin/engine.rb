2289adce6b080240a68448db06e7cf591afb3a4a
geodemeetslivia <gkontesidou@gmail.com>
2017-01-05 16:03:42 +0200

    upgrade rollout_admin

--- lib/rollout_admin/engine.rb
+++ lib/rollout_admin/engine.rb
@@ -2,8 +2,6 @@ module RolloutAdmin
   class Engine < ::Rails::Engine
     isolate_namespace RolloutAdmin
     require 'jquery-ui-rails'
-    require 'less-rails-bootstrap'
-
     initializer "rollout_admin.init_task", :before=> :load_config_initializers do |app|
     	puts "running the initializer, nothing done here"
     end
